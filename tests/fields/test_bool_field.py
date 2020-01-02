dd3c91a698263dd6ca9cbb41224103a7d387b7f4
Corentin Séchet <gdaow@users.noreply.github.com>
2020-01-02 13:49:24 +0100

    Made a specific test class for dict field tests.

--- tests/fields/test_bool_field.py
+++ tests/fields/test_bool_field.py
@@ -14,6 +14,7 @@ class _BoolObject:
 
         bool_field = BoolField()
 
+
 def test_bool_field():
     """Test bool field loading works."""
     true_values = [
--- tests/fields/test_dict_field.py
+++ tests/fields/test_dict_field.py
@@ -1,14 +1,22 @@
 """Dictionary field tests."""
+from pofy import DictField
 from pofy import ErrorCode
+from pofy import StringField
 from pofy import load
 
-from tests.fixtures import YamlObject
 from tests.fixtures import expect_load_error
+
+
+class _DictObject:
+    class Schema:
+        """Pofy fields."""
+
+        dict_field = DictField(StringField())
 
 
 def test_dict_field():
     """Test dict field loading works."""
-    test = load(YamlObject, (
+    test = load(_DictObject, (
         'dict_field:\n' +
         '  key_1: value_1\n' +
         '  key_2: value_2'
@@ -20,7 +28,7 @@ def test_dict_field_error_on_bad_node():
     """Test dict field loading raises an error on bad node."""
     expect_load_error(
         ErrorCode.UNEXPECTED_NODE_TYPE,
-        YamlObject,
+        _DictObject,
         'dict_field:\n' +
         '  - key_1\n' +
         '  - key_2'
