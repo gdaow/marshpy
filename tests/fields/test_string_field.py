ba2332504cb21edb2e9f79cea078d7cb1c28ea13
Corentin Séchet <gdaow@users.noreply.github.com>
2020-01-02 13:53:31 +0100

    Made a specific test class for string field.

--- tests/fields/test_string_field.py
+++ tests/fields/test_string_field.py
@@ -1,14 +1,23 @@
 """String field tests."""
-from pofy import load
 from pofy import ErrorCode
+from pofy import StringField
+from pofy import load
 
-from tests.fixtures import YamlObject
 from tests.fixtures import expect_load_error
+
+
+class _StringObject:
+
+    class Schema:
+        """Pofy fields."""
+
+        string_field = StringField()
+        match_string_field = StringField(pattern='^Matching$')
 
 
 def test_string_field():
     """Test string field loading works."""
-    test = load(YamlObject, 'string_field: test_value')
+    test = load(_StringObject, 'string_field: test_value')
     assert test.string_field == 'test_value'
 
 
@@ -16,7 +25,7 @@ def test_bad_value_raises():
     """Test not-scalar node for a string field raise an error."""
     expect_load_error(
         ErrorCode.UNEXPECTED_NODE_TYPE,
-        YamlObject,
+        _StringObject,
         'string_field: ["a", "list"]'
     )
 
@@ -24,13 +33,13 @@ def test_bad_value_raises():
 def test_pattern():
     """Test string validates pattern when given."""
     test = load(
-        YamlObject,
+        _StringObject,
         'match_string_field: Matching'
     )
     assert test.match_string_field == 'Matching'
 
     expect_load_error(
         ErrorCode.VALIDATION_ERROR,
-        YamlObject,
+        _StringObject,
         'match_string_field: NotMatching'
     )
