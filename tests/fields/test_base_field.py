7c82d549fc39c5f5881f09821f28ca7c6f4ebd0e
Corentin Séchet <gdaow@users.noreply.github.com>
2020-01-02 14:28:42 +0100

    Removed obsolete fields, moved test objects.

--- tests/fields/test_base_field.py
+++ tests/fields/test_base_field.py
@@ -1,56 +1,26 @@
 """Yaml object loading tests."""
-from io import StringIO
-
-from yaml import compose
-
 from pofy import ErrorCode
-from pofy import Resolver
-from pofy import load
+from pofy import LoadingContext
+from pofy import StringField
 
-from tests.fixtures import YamlObject
 from tests.fixtures import expect_load_error
-
-
-class _DummyResolver(Resolver):
-    def resolve(self, location):
-        if location == 'good_location':
-            return compose(StringIO('test_field: test_value'))
-        return None
-
-
-def test_include():
-    """Test !include tag uses resolver to find YAML documents to load."""
-    test = load(
-        YamlObject,
-        'object_field: !include good_location',
-        resolvers=[_DummyResolver()]
-    )
-    assert test.object_field.test_field == 'test_value'
-
-
-def test_include_on_bad_node_raise_error():
-    """Test !include tag on a not-scalar node raises an error."""
-    expect_load_error(
-        ErrorCode.UNEXPECTED_NODE_TYPE,
-        YamlObject,
-        'object_field: !include [me, is, a, list]'
-    )
-
-
-def test_unresolved_include_raise_error():
-    """Test include not found raises an error."""
-    expect_load_error(
-        ErrorCode.INCLUDE_NOT_FOUND,
-        YamlObject,
-        'object_field: !include bad_location',
-        resolvers=[_DummyResolver()]
-    )
 
 
 def test_field_validation():
     """Test custom field validaton works."""
+    def _validate(context: LoadingContext, _: str):
+        context.error(ErrorCode.VALIDATION_ERROR, 'Test')
+        return False
+
+    class _ValidateFieldObject:
+
+        class Schema:
+            """Pofy fields."""
+
+            validated_field = StringField(validate=_validate)
+
     expect_load_error(
         ErrorCode.VALIDATION_ERROR,
-        YamlObject,
+        _ValidateFieldObject,
         'validated_field: error_is_always_raised',
     )
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,33 +1,41 @@
 """Yaml object loading tests."""
-from pofy import load
 from pofy import ErrorCode
+from pofy import ObjectField
+from pofy import StringField
+from pofy import load
 
-from .fixtures import RequiredFieldObject
-from .fixtures import SubObject
-from .fixtures import SubObjectChild
-from .fixtures import ValidatedObjectChild
-from .fixtures import YamlObject
 from .fixtures import expect_load_error
 
 
 def test_unknown_field_raise_error():
     """Test an undeclared field in YAML raises an error."""
+    class _EmptyObject:
+        class Schema:
+            """Pyfo fields."""
+
     expect_load_error(
         ErrorCode.FIELD_NOT_DECLARED,
-        YamlObject,
+        _EmptyObject,
         'uknown_field: 10'
     )
 
 
 def test_unset_required_field_raise_error():
     """Test an unset required field in YAML raise an error."""
+    class _RequiredFieldObject:
+        class Schema:
+            """Pofy fields."""
+
+            required = StringField(required=True)
+            not_required = StringField(required=True)
+
     expect_load_error(
         ErrorCode.MISSING_REQUIRED_FIELD,
-        RequiredFieldObject,
+        _RequiredFieldObject,
         'not_required: some_value'
     )
 
-    test = load(RequiredFieldObject, (
+    test = load(_RequiredFieldObject, (
         'required: setted\n'
         'not_required: yodeldi'
     ))
@@ -37,20 +45,39 @@ def test_unset_required_field_raise_error():
 
 def test_load_subclass():
     """Test fields declared in parent classes are loaded."""
+    class _Parent:
+        class Schema:
+            """Pyfo fields."""
+
+            parent_field = StringField()
+
+    class _Child(_Parent):
+        class Schema:
+            """Pyfo fields."""
+
+            child_field = StringField()
+
     test = load(
-        SubObjectChild,
-        'test_field: parent_value\n' +
+        _Child,
+        'parent_field: parent_value\n' +
         'child_field: child_value',
     )
-    assert test.test_field == 'parent_value'
+
+    assert test.parent_field == 'parent_value'
     assert test.child_field == 'child_value'
 
 
 def test_error_on_bad_node():
     """Test load raises an error on bad node."""
+    class _DummyObject:
+        class Schema:
+            """Pyfo fields."""
+
+            some_field = StringField()
+
     expect_load_error(
         ErrorCode.UNEXPECTED_NODE_TYPE,
-        YamlObject,
+        _DummyObject,
         '- item1\n' +
         '- item2'
     )
@@ -58,20 +85,59 @@ def test_error_on_bad_node():
 
 def test_resolve_root_works(datadir):
     """Test giving a path as resolve_root instanciates FileSystemResolvers."""
+    class _Owned:
+        class Schema:
+            """Pyfo fields."""
+
+            test_field = StringField()
+
+    class _Owner:
+        class Schema:
+            """Pyfo fields."""
+
+            object_field = ObjectField(object_class=_Owned)
+
     test = load(
-        YamlObject,
+        _Owner,
         'object_field: !include object.yaml\n',
         resolve_roots=[datadir]
     )
 
-    assert isinstance(test.object_field, SubObject)
+    assert isinstance(test.object_field, _Owned)
     assert test.object_field.test_field == 'test_value'
 
 
 def test_object_validation_works():
     """Test object validation methods are executed."""
+
+    class _ValidatedObject:
+        class Schema:
+            """Pofy fields."""
+
+            dont_set_me = StringField()
+
+            @classmethod
+            def validate(cls, context, obj):
+                """Validate loaded objects."""
+                if obj.dont_set_me is not None:
+                    context.error(ErrorCode.VALIDATION_ERROR, 'Error')
+                return False
+
+        def __init__(self):
+            """Initialize."""
+            self.dont_set_me = None
+
+    class _ValidatedObjectChild(_ValidatedObject):
+        class Schema:
+            """Pofy fields."""
+
+            @classmethod
+            def validate(cls, __, ___):
+                """Validate loaded objects."""
+                return True
+
     expect_load_error(
         ErrorCode.VALIDATION_ERROR,
-        ValidatedObjectChild,
+        _ValidatedObjectChild,
         'dont_set_me: Wathever'
     )
