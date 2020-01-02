12b605f371c2150a98aa2e9977bef63a3ae4848f
Corentin Séchet <gdaow@users.noreply.github.com>
2020-01-02 13:54:49 +0100

    Made a specific class for int field tests.

--- tests/fields/test_int_field.py
+++ tests/fields/test_int_field.py
@@ -1,20 +1,30 @@
 """Integer field tests."""
 from pofy import ErrorCode
+from pofy import IntField
 from pofy import load
 
-from tests.fixtures import YamlObject
 from tests.fixtures import expect_load_error
+
+
+class _IntObject:
+
+    class Schema:
+        """Pofy fields."""
+
+        int_field = IntField()
+        hex_int_field = IntField(base=16)
+        bounded_int_field = IntField(minimum=10, maximum=20)
 
 
 def test_int_field():
     """Test integer field loading works."""
-    test = load(YamlObject, 'int_field: 10')
+    test = load(_IntObject, 'int_field: 10')
     assert test.int_field == 10
 
-    test = load(YamlObject, 'int_field: 0xF00D00FAFA')
+    test = load(_IntObject, 'int_field: 0xF00D00FAFA')
     assert test.int_field == 0xF00D00FAFA
 
-    test = load(YamlObject, 'int_field: 0o42')
+    test = load(_IntObject, 'int_field: 0o42')
     assert test.int_field == 0o42
 
 
@@ -22,14 +32,14 @@ def test_int_field_bad_value_raises():
     """Test integer field bad value raises an error."""
     expect_load_error(
         ErrorCode.VALUE_ERROR,
-        YamlObject,
+        _IntObject,
         'int_field: not_convertible'
     )
 
 
 def test_int_field_base():
     """Test integer field base parameter works."""
-    test = load(YamlObject, 'hex_int_field: F00D00FAFA')
+    test = load(_IntObject, 'hex_int_field: F00D00FAFA')
     assert test.hex_int_field == 0xF00D00FAFA
 
 
@@ -37,12 +47,12 @@ def test_int_field_min_max():
     """Test integer field minimum / maximum parameter works."""
     expect_load_error(
         ErrorCode.VALIDATION_ERROR,
-        YamlObject,
+        _IntObject,
         'bounded_int_field: 0'
     )
 
     expect_load_error(
         ErrorCode.VALIDATION_ERROR,
-        YamlObject,
+        _IntObject,
         'bounded_int_field: 100'
     )
