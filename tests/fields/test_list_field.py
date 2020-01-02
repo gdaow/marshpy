2ad72b054ab8e4473de82cd7f4e25649756c2ffc
Corentin Séchet <gdaow@users.noreply.github.com>
2020-01-02 13:56:11 +0100

    Made a specific test class for list field.

--- tests/fields/test_list_field.py
+++ tests/fields/test_list_field.py
@@ -1,14 +1,23 @@
 """List field tests."""
 from pofy import ErrorCode
+from pofy import ListField
+from pofy import StringField
 from pofy import load
 
-from tests.fixtures import YamlObject
 from tests.fixtures import expect_load_error
+
+
+class _ListObject:
+
+    class Schema:
+        """Pofy fields."""
+
+        list_field = ListField(StringField())
 
 
 def test_list_field():
     """Test list field loading works."""
-    test = load(YamlObject, 'list_field: ["value_1", "value_2" ]')
+    test = load(_ListObject, 'list_field: ["value_1", "value_2" ]')
     assert test.list_field == ['value_1', 'value_2']
 
 
@@ -16,7 +25,7 @@ def test_list_field_error_on_bad_node():
     """Test list field loading raises an error on bad node."""
     expect_load_error(
         ErrorCode.UNEXPECTED_NODE_TYPE,
-        YamlObject,
+        _ListObject,
         'list_field:\n' +
         '  key_1: value_1\n' +
         '  key_2: value_2'
