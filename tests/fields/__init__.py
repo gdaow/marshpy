a1f6586ddbdfb8f64f0b53b3f1f6994a035d3099
Corentin Séchet <gdaow@users.noreply.github.com>
2020-01-02 14:08:50 +0100

    Made a specific test class for object field.

---
+++ tests/fields/__init__.py
@@ -0,0 +1,1 @@
+"""Field related tests."""
--- tests/fields/test_object_field.py
+++ tests/fields/test_object_field.py
@@ -1,20 +1,43 @@
 """Object field tests."""
-from pofy import load
 from pofy import ErrorCode
+from pofy import ObjectField
+from pofy import StringField
+from pofy import load
 
-from tests.fixtures import SubObject
-from tests.fixtures import SubObjectChild
-from tests.fixtures import YamlObject
 from tests.fixtures import expect_load_error
+
+
+class _SubObject:
+
+    class Schema:
+        """Pofy fields."""
+
+        test_field = StringField()
+
+
+class _SubObjectChild(_SubObject):
+
+    class Schema:
+        """Pofy fields."""
+
+        child_field = StringField()
+
+
+class _SubObjectOwner:
+
+    class Schema:
+        """Pofy fields."""
+
+        object_field = ObjectField(object_class=_SubObject)
 
 
 def test_object_field():
     """Test object field loading works."""
-    test = load(YamlObject, (
+    test = load(_SubObjectOwner, (
         'object_field:\n' +
         '  test_field: field_value\n'
     ))
-    assert isinstance(test.object_field, SubObject)
+    assert isinstance(test.object_field, _SubObject)
     assert test.object_field.test_field == 'field_value'
 
 
@@ -22,7 +45,7 @@ def test_object_field_error_on_bad_node():
     """Test object field loading raises an error on bad node."""
     expect_load_error(
         ErrorCode.UNEXPECTED_NODE_TYPE,
-        YamlObject,
+        _SubObjectOwner,
         'object_field:\n' +
         '  - item1\n' +
         '  - item2'
@@ -31,12 +54,13 @@ def test_object_field_error_on_bad_node():
 
 def test_type_tag():
     """Test specifying a type for an object field works."""
-    test = load(YamlObject, (
-        'object_field: !type:tests.fixtures.SubObjectChild\n' +
+    test = load(
+        _SubObjectOwner,
+        'object_field: !type:tests.fields.test_object_field._SubObjectChild\n' +
         '  test_field: test_value\n' +
         '  child_field: child_value\n'
-    ))
-    assert isinstance(test.object_field, SubObjectChild)
+    )
+    assert isinstance(test.object_field, _SubObjectChild)
     assert test.object_field.test_field == 'test_value'
     assert test.object_field.child_field == 'child_value'
 
@@ -45,7 +69,7 @@ def test_bad_type_raise_error():
     """Test a type tag not existing or not pointing to a type raises error."""
     expect_load_error(
         ErrorCode.TYPE_RESOLVE_ERROR,
-        YamlObject,
+        _SubObjectOwner,
         'object_field: !type:tests.fixtures.IDontExist\n' +
         '  test_field: test_value\n' +
         '  child_field: child_value\n'
@@ -53,7 +77,7 @@ def test_bad_type_raise_error():
 
     expect_load_error(
         ErrorCode.TYPE_RESOLVE_ERROR,
-        YamlObject,
+        _SubObjectOwner,
         'object_field: !type:tests.fixtures.expect_load_error\n' +
         '  test_field: test_value\n' +
         '  child_field: child_value\n'
@@ -64,7 +88,7 @@ def test_bad_formatted_type_tag():
     """Test an error is raised for badly formatted type tags."""
     expect_load_error(
         ErrorCode.BAD_TYPE_TAG_FORMAT,
-        YamlObject,
+        _SubObjectOwner,
         (
             'object_field: !typetests.fixtures.SubObjectChild\n' +
             '  test_field: test_value'
@@ -73,7 +97,7 @@ def test_bad_formatted_type_tag():
 
     expect_load_error(
         ErrorCode.BAD_TYPE_TAG_FORMAT,
-        YamlObject,
+        _SubObjectOwner,
         (
             'object_field: !type:tests:fixtures.SubObjectChild\n' +
             '  test_field: test_value'
@@ -82,7 +106,7 @@ def test_bad_formatted_type_tag():
 
     expect_load_error(
         ErrorCode.BAD_TYPE_TAG_FORMAT,
-        YamlObject,
+        _SubObjectOwner,
         (
             'object_field: !type:tests\n' +
             '  test_field: test_value'
