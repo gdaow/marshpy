b8821380e10afbc237348dd91f26994e88c2dae5
Corentin Séchet <gdaow@users.noreply.github.com>
2020-01-04 12:32:38 +0100

    Added type hints to bool test unit test.

--- tests/fields/test_bool_field.py
+++ tests/fields/test_bool_field.py
@@ -1,4 +1,6 @@
 """String field tests."""
+from typing import Any
+
 from pofy import BoolField
 from pofy import ErrorCode
 
@@ -15,15 +17,15 @@ class _BoolObject:
         bool_field = BoolField()
 
 
-def _check_bool_field(value, expected_value):
-    check_field(_BoolObject, 'bool_field', value, expected_value)
+def _check_bool_field(yaml_value: str, expected_value: bool):
+    check_field(_BoolObject, 'bool_field', yaml_value, expected_value)
 
 
-def _check_bool_field_error(value, expected_error):
-    check_field_error(_BoolObject, 'bool_field', value, expected_error)
+def _check_bool_field_error(yaml_value: str, expected_error: ErrorCode):
+    check_field_error(_BoolObject, 'bool_field', yaml_value, expected_error)
 
 
-def test_bool_field():
+def test_bool_field() -> Any:
     """Test bool field loads correct values."""
     true_values = [
         'y', 'Y', 'yes', 'Yes', 'YES',
@@ -44,7 +46,7 @@ def test_bool_field():
         _check_bool_field(value, False)
 
 
-def test_bool_field_error_handling():
+def test_bool_field_error_handling() -> Any:
     """Test BoolField error handling behaves correctly."""
     _check_bool_field_error('NotValidValue', ErrorCode.VALUE_ERROR)
     _check_bool_field_error('["a", "list"]', ErrorCode.UNEXPECTED_NODE_TYPE)
