92dfb01fb7f128d5f6e1ce7baa8361698f76edde
Corentin Séchet <gdaow@users.noreply.github.com>
2020-01-05 01:22:08 +0100

    Cleaned env tag tests.

--- tests/tag_handlers/test_env_handler.py
+++ tests/tag_handlers/test_env_handler.py
@@ -1,5 +1,9 @@
-"""Glob handler tests."""
+"""Env handler tests."""
 from os import environ
+from typing import Any
+from typing import Type
+
+from yaml import MappingNode
 from yaml import ScalarNode
 from yaml import SequenceNode
 
@@ -10,27 +14,37 @@ from pofy.common import LOADING_FAILED
 from tests.helpers import load_node
 
 
-def test_bad_node_for_env_raises():
-    """Test glob tag on a not-scalar node raise an error."""
-    load_node(
-        node=SequenceNode('!env', '', None, None),
-        expected_error=ErrorCode.UNEXPECTED_NODE_TYPE,
+def _check_tag(node_value: str, expected: Any):
+    result = load_node(
+        node=ScalarNode('!env', node_value, None, None),
         tag_handlers=[EnvHandler()]
     )
+    assert result == expected
 
 
-def test_env_tag_handler():
-    """Test glob resolves correctly files."""
-    environ['TEST_VARIABLE'] = 'test_value'
+def _check_tag_error(
+    node_value: str,
+    node_type: Type = ScalarNode,
+    expected_error: ErrorCode = None
+):
     result = load_node(
-        node=ScalarNode('!env', 'TEST_VARIABLE', None, None),
-        tag_handlers=[EnvHandler()]
-    )
-
-    assert result == 'test_value'
-
-    result = load_node(
-        node=ScalarNode('!env', 'I_HOPE_YOU_DIDNT_SET_THIS_ONE', None, None),
+        node=node_type('!env', node_value, None, None),
+        expected_error=expected_error,
         tag_handlers=[EnvHandler()]
     )
     assert result == LOADING_FAILED
+
+
+def test_env_tag():
+    """Env tag should correctly load environment variables."""
+    environ['TEST_VARIABLE'] = 'test_value'
+    _check_tag('TEST_VARIABLE', 'test_value')
+
+
+def test_env_tag_error_handling():
+    """Env tag should handle errors correctly."""
+    _check_tag_error([], SequenceNode, ErrorCode.UNEXPECTED_NODE_TYPE)
+    _check_tag_error([], MappingNode, ErrorCode.UNEXPECTED_NODE_TYPE)
+
+    # Shouldn't emit an error
+    _check_tag('I_HOPE_YOU_DIDNT_SET_THIS_ONE', LOADING_FAILED)
