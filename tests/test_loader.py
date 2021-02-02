5eca5881c3526d168d49a4d6012b78b4aa04864e
Corentin Séchet <gdaow@users.noreply.github.com>
2021-02-02 09:19:19 +0100

    Testing schema_resolver overriding.

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -140,3 +140,32 @@ def test_error_hanlder_is_called() -> None:
 
     load('[a, list]', str, error_handler=_handler)
     assert handler_called
+
+
+def test_schema_resolver_is_called() -> None:
+    """The given schema_resolver should be called when defined."""
+    resolver_called = False
+
+    class _Schema:
+        string_field = StringField()
+
+    class _Object:
+        def __init__(self) -> None:
+            self.string_field = 'default value'
+
+    def _schema_resolver(cls: Type[Any]) -> Optional[Type[Any]]:
+        nonlocal resolver_called
+        if cls == _Object:
+            resolver_called = True
+            return _Schema
+        return None
+
+    result = load(
+        'string_field: value',
+        object_class=_Object,
+        schema_resolver=_schema_resolver
+    )
+
+    assert resolver_called
+    assert isinstance(result, _Object)
+    assert result.string_field == 'value'
